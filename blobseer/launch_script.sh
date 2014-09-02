d1dde2337de3e02731f0daf959f6e8537a19ae98
Rohit Saxena <aerohit.saxena@gmail.com>
2014-09-02 13:08:45 +0200

    Deleted blobseer related files.

--- blobseer/launch_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pmanager/pmanager test/blobseer-deploy.cfg &
-provider/provider test/blobseer-deploy.cfg &
-provider/sdht test/blobseer-deploy.cfg &
-vmanager/vmanager test/blobseer-deploy.cfg &
