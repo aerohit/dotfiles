4dc209be1dfb11a1cfaed423f6ef977c1b6aa6f0
Rohit Saxena <aerohit.saxena@gmail.com>
2014-09-30 13:31:09 +0200

    Removed brew plugin from fish.

--- fish/config.fish
+++ fish/config.fish
@@ -7,10 +7,16 @@ set fish_theme robbyrussell
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins brew bundler jump rails rvm sublime tmux vi-mode vundle
+set fish_plugins bundler jump rails rbenv sublime tmux vi-mode vundle
 
 # Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
+set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
+
+set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+set -x SBT_OPTS "-XX:+CMSClassUnloadingEnabled -XX:PermSize=1024M -XX:MaxPermSize=2048M"
+
+set -gx RBENV_ROOT /usr/local/var/rbenv
+. (rbenv init -|psub)
