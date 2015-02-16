03545d09316e5d6a4f866c90972027a521840fa9
Rohit Saxena <aerohit.saxena@gmail.com>
2015-02-16 17:46:33 +0100

    updated note taking functions to use nvim

--- fish/functions/n.fish
+++ fish/functions/n.fish
@@ -1,3 +1,3 @@
 function n
-    vim -c :Note
+    nvim -c :Note
 end
--- fish/functions/ne.fish
+++ fish/functions/ne.fish
@@ -1,6 +1,6 @@
 function ne
     if test (count $argv) -gt 0
-        vim -c ":Note $argv"
+        nvim -c ":Note $argv"
     else
         echo "Please provide file name (use nls)"
     end
