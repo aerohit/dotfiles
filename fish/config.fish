fa8f5032eec45414e9bab62ad4e1910f57cd45a9
Rohit Saxena <aerohit.saxena@gmail.com>
2015-02-18 08:00:33 +0100

    Added a few functions to fish and changed config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,10 +7,10 @@ set fish_theme robbyrussell
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins bundler jump rails rbenv sublime tmux vi-mode vundle
+set autojump brew bundler fish_plugins jump rails rbenv rvm sublime tmux vi-mode vundle
 
 # Path to your custom folder (default path is $FISH/custom)
-set fish_custom $HOME/dotfiles/oh-my-fish
+#set fish_custom $HOME/dotfiles/fish
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
@@ -20,3 +20,9 @@ set -x SBT_OPTS "-XX:+CMSClassUnloadingEnabled -XX:PermSize=1024M -XX:MaxPermSiz
 
 set -gx RBENV_ROOT /usr/local/var/rbenv
 . (rbenv init -|psub)
+
+# Add GHC 7.8.3 to the PATH, via http://ghcformacosx.github.io/
+set -x GHC_DOT_APP "/Applications/ghc-7.8.3.app"
+#add-to-path "$HOME/.cabal/bin" "$GHC_DOT_APP/Contents/bin"
+set -x PATH $PATH ~/.cabal/bin
+set -x PATH $PATH "$GHC_DOT_APP/Contents/bin"
