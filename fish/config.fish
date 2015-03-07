9b1c8f9eaa99e3745b8f2e0c52a6c33328cc3534
Rohit Saxena <aerohit.saxena@gmail.com>
2015-03-07 13:31:22 -0500

    Fixed broken config

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,7 @@ set fish_theme robbyrussell
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-<<<<<<< Updated upstream
 set fish_plugins autojump brew bundler jump rails rvm sublime tmux vi-mode vundle
-=======
-set fish_plugins bundler jump git rails sublime tmux vundle
->>>>>>> Stashed changes
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/fish
@@ -22,7 +18,6 @@ set fish_plugins bundler jump git rails sublime tmux vundle
 #set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
 set -x SBT_OPTS "-XX:+CMSClassUnloadingEnabled -XX:PermSize=1024M -XX:MaxPermSize=2048M"
 
-<<<<<<< Updated upstream
 # Add GHC 7.8.3 to the PATH, via http://ghcformacosx.github.io/
 if test -f /Applications/ghc-7.8.3.app
     set -x GHC_DOT_APP "/Applications/ghc-7.8.3.app"
@@ -30,7 +25,3 @@ if test -f /Applications/ghc-7.8.3.app
 end
 set -gx PATH ~/.cabal/bin /opt/cabal/1.20/bin /opt/ghc/7.8.3/bin /opt/happy/1.19.4/bin /opt/alex/3.1.3/bin $PATH
 set -gx PATH ~/bin $PATH
-=======
-set -gx PATH ~/.cabal/bin /opt/cabal/1.20/bin /opt/ghc/7.8.3/bin /opt/happy/1.19.4/bin /opt/alex/3.1.3/bin $PATH
-#. (rbenv init -|psub)
->>>>>>> Stashed changes
