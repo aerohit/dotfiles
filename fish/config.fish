377e54c16ad7a570a06d483514f1561e2a403af0
Rohit Saxena <aerohit.saxena@gmail.com>
2015-08-18 13:24:56 +0200

    New style configuration for fish.

--- fish/config.fish
+++ fish/config.fish
@@ -1,30 +1,31 @@
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 
-# Theme
-set fish_theme robbyrussell
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-set fish_plugins autojump brew bundler jump rails rvm sublime tmux vi-mode vundle
-
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/fish
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
-#set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
-set -x SBT_OPTS "-XX:+CMSClassUnloadingEnabled -XX:PermSize=1024M -XX:MaxPermSize=2048M"
-
-# Add GHC 7.8.3 to the PATH, via http://ghcformacosx.github.io/
-if test -f /Applications/ghc-7.8.3.app
-    set -x GHC_DOT_APP "/Applications/ghc-7.8.3.app"
-    set -x PATH $PATH "$GHC_DOT_APP/Contents/bin"
+if test -z $rvm_bin_path
+  exec bash --login -c "exec fish" ^&1
 end
-set -gx PATH ~/.cabal/bin /opt/cabal/1.20/bin /opt/ghc/7.8.3/bin /opt/happy/1.19.4/bin /opt/alex/3.1.3/bin $PATH
-set -gx PATH ~/bin $PATH
+
+# Custom plugins and themes may be added to ~/.oh-my-fish/custom
+# Plugins and themes can be found at https://github.com/oh-my-fish/
+Theme 'robbyrussell'
+Plugin 'theme'
+Plugin 'brew'
+Plugin 'bundler'
+Plugin 'jump'
+Plugin 'rails'
+Plugin 'rvm'
+Plugin 'sublime'
+Plugin 'tmux'
+Plugin 'vi-mode'
+Plugin 'vundle'
+
+set -x PLAY_LOG_FILE "-Dlogger.file=conf/logger-dev.xml"
 
 alias tmls="tmux list-sessions"
 alias tmas="tmux attach-session -t"
@@ -34,3 +35,13 @@ alias tmksr="tmux kill-server"
 alias tmkss="tmux kill-session"
 alias vim="env NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim"
 alias nvim="env NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim"
+
+set -x LC_ALL "en_US.UTF-8"
+set -x LC_COLLATE "en_US.UTF-8"
+set -x LC_CTYPE "en_US.UTF-8"
+set -x LC_MESSAGES "en_US.UTF-8"
+set -x LC_MONETARY "en_US.UTF-8"
+set -x LC_NUMERIC "en_US.UTF-8"
+set -x LC_TIME "en_US.UTF-8"
+
+set -x TMUX_POWERLINE_PATCHED_FONT_IN_USE "false"
