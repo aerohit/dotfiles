27a3de1087137b4bd863a79f5617f9ccb34cc941
Rohit Saxena <aerohit.saxena@gmail.com>
2016-08-02 12:36:39 +0200

    Some fish shell configs.

--- fish/config.fish
+++ fish/config.fish
@@ -25,7 +25,7 @@ Plugin 'tmux'
 Plugin 'vi-mode'
 Plugin 'vundle'
 
-set -x PLAY_LOG_FILE "-Dlogger.file=conf/logger-dev.xml"
+set -x PLAY_LOG_FILE "-Dlogger.file=/Users/rohit/codes/site/customer-site/conf/logger-dev.xml"
 
 alias tmls="tmux list-sessions"
 alias tmas="tmux attach-session -t"
@@ -45,3 +45,16 @@ set -x LC_NUMERIC "en_US.UTF-8"
 set -x LC_TIME "en_US.UTF-8"
 
 set -x TMUX_POWERLINE_PATCHED_FONT_IN_USE "false"
+
+set -x PIP_REQUIRE_VIRTUALENV true
+set -x PYTHONPATH "~/codes/pymodules"
+set -x WORKON_HOME "~/codes/virtualenvs"
+
+set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home"
+set -x M2_HOME "/usr/local/Cellar/maven/3.3.3/libexec"
+set -gx PATH $NGINX/sbin /usr/local/opt/php56/bin/ /usr/local/sbin /usr/local/bin $M2_HOME/bin $JAVA_HOME/bin $PATH
+
+set -gx PATH "/Users/rohit/wahanda/gitflow" $PATH
+set -gx PATH "/usr/local/opt/coreutils/libexec/gnubin" $PATH
+set -gx PATH "/Users/rohit/.local/bin" $PATH
+set -gx NODE_TLS_REJECT_UNAUTHORIZED 0
