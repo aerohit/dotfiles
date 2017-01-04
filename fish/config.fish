2a068010f16693cf94e14dd98bc3cbc03493a968
Rohit Saxena <aerohit.saxena@gmail.com>
2017-01-04 16:50:35 +0100

    Configured JAVA_HOME

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,10 @@
+# Paths
+
+set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_112.jdk/Contents/Home"
+set -gx PATH $JAVA_HOME/bin $PATH
+
+# Aliases
+
 alias tmls="tmux list-sessions"
 alias tmas="tmux attach-session -t"
 alias tmns="tmux new-session -s"
