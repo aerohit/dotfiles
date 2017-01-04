d7bfe944d56e7442ba17ada3975e98a0aa293a08
Rohit Saxena <aerohit.saxena@gmail.com>
2017-01-04 12:22:05 +0100

    Created aliases for tmux and vim

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,9 @@
-alias vim=nvim
+alias tmls="tmux list-sessions"
+alias tmas="tmux attach-session -t"
+alias tmns="tmux new-session -s"
+alias tmss="tmux switch -t"
+alias tmksr="tmux kill-server"
+alias tmkss="tmux kill-session"
+
+alias vim="env NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim"
+alias nvim="env NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim"
