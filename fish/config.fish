23cba04519bd39d304a0a01e67952c24c91a5d12
Rohit Saxena <aerohit.saxena@gmail.com>
2017-03-28 11:27:06 +0200

    Fish changes.

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,16 @@
 
 set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_112.jdk/Contents/Home"
 set -gx PATH $JAVA_HOME/bin $PATH
+set -gx SBT_OPTS "-Xmx2G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled  -XX:+HeapDumpOnOutOfMemoryError"
+
+# Docker
+#eval (docker-machine env default)
 
 # Aliases
 
 alias tmls="tmux list-sessions"
+alias tmlw="tmux list-windows -a"
+alias tmlp="tmux list-panes -a"
 alias tmas="tmux attach-session -t"
 alias tmns="tmux new-session -s"
 alias tmss="tmux switch -t"
